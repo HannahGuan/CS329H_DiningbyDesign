cb7455ab55d2f70f7c0fc213744c1ac3a97ae244
HannahGuan <69283640+HannahGuan@users.noreply.github.com>
2025-11-20 12:47:29 -0800

    Revise restaurant profile generation prompt
    Updated the RESARUANTS_PROFILE_GENERATION_PROMPT to provide detailed instructions for generating restaurant descriptions.

--- profile_generation/constants.py
+++ profile_generation/constants.py
@@ -1,4 +1,12 @@
 USER_PROFILE_GENERATION_PROMPT = """fill in"""
 
 
-RESARUANTS_PROFILE_GENERATION_PROMPT = """fill in"""
+RESARUANTS_PROFILE_GENERATION_PROMPT = """You are a summarizer to elaborate on the fragmented information about a restaurant. Your goal is to generate a fluent description of a business restaurant based on the given json-like description. 
+
+<|The Start of JSON Information>
+{json_info}
+<|The End of JSON Information|>
+
+# Guidelines:
+1. Your response should be only one paragraph
+2. You should not mention stars and ‘other reviewers’ explicitly. Instead, your style should sound like a neutral description."""
